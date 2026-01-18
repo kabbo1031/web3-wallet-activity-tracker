107b21a32a989b6f6c3db78b494dcd6d10662a7c
Parmita <160781867+kabbo1031@users.noreply.github.com>
2026-01-18 14:09:20 +0600

    Create wallet.registry.js

---
+++ wallet.registry.js
@@ -0,0 +1,14 @@
+export function loadWallets() {
+  console.log("Loading wallets...");
+
+  return [
+    {
+      address: "0xWALLET001",
+      balance: 500
+    },
+    {
+      address: "0xWALLET002",
+      balance: 200
+    }
+  ];
+}
